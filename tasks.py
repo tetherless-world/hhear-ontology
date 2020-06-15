6643d6cb29d27192a0be0e8a773de1d63b062691
Jim McCusker <mccusker@gmail.com>
2020-06-15 18:12:24 -0400

    Added support to pre-process SDDs with 'other' codes.

--- tasks.py
+++ tasks.py
@@ -21,8 +21,10 @@ SETL_FILE='ontology.setl.ttl'
 ontology_setl = Namespace('https://hadatac.org/setl/')
 setl = Namespace('http://purl.org/twc/vocab/setl/')
 prov = Namespace('http://www.w3.org/ns/prov#')
+skos = Namespace('http://www.w3.org/2004/02/skos/core#')
 dc = Namespace('http://purl.org/dc/terms/')
 pv = Namespace('http://purl.org/net/provenance/ns#')
+csvw = Namespace('http://www.w3.org/ns/csvw#')
 
 logging_level = logging.INFO
 logging.basicConfig(level=logging_level)
@@ -102,5 +104,93 @@ def chear2hhear(c, inputfile, outputfile):
                             c = mappings[c]
                         cellValues.append(c)
                     cell.value = ', '.join(cellValues)
+
+    wb.save(outputfile)
+
+@task
+def sdd2owl(c, inputfile, ontology, outputfile, infosheet="InfoSheet"):
+    import openpyxl
+    import re
+    import pandas as pd
+    cwd = os.getcwd()
+
+    setl_graph = Graph()
+    setl_graph.parse('sdd_owl_semantics.setl.ttl',format="turtle")
+
+    sddns = rdflib.Namespace('http://purl.org/twc/sdd/setl/')
+
+    tab_config = {
+        "Data_Dictionary" : sddns.dm_table,
+        "Codebook" : sddns.codebook_table,
+        "Code_Mappings" : sddns.codemapping_table,
+        "Timeline" : sddns.timeline_table
+    }
+
+    wb = openpyxl.load_workbook(inputfile)
+
+    infosheet_tab = wb[infosheet]
+    infosheet_dict = dict([(row[0].value,row[1].value) for row in infosheet_tab])
+
+    infosheet_resource = setl_graph.resource(sddns.info_sheet)
+    infosheet_resource.add(rdflib.RDF.type, setl.Excel)
+    infosheet_resource.add(setl.sheetname, rdflib.Literal(infosheet))
+    gen = infosheet_resource.value(prov.wasGeneratedBy)
+    gen.add(prov.used, URIRef('file://'+os.path.join(cwd,inputfile)))
+
+    for entry, uri in tab_config.items():
+        res = setl_graph.resource(uri)
+        sheet = infosheet_dict[entry]
+        gen = res.value(prov.wasGeneratedBy)
+        if sheet.startswith('#'):
+            res.add(rdflib.RDF.type, setl.Excel)
+            res.add(setl.sheetname, rdflib.Literal(sheet[1:]))
+            gen.add(prov.used, URIRef('file://'+os.path.join(cwd,inputfile)))
+        else:
+            res.add(rdflib.RDF.type, csvw.Table)
+            gen.add(prov.used, URIRef(sheet))
+
+    resources = setlr._setl(setl_graph)
+
+    ont = resources[sddns.metadata]
+
+    prefix = "http://purl.org/twc/ctxid/"
+
+    mappings = {}
+
+    for cls in ont.subjects(rdflib.RDF.type, rdflib.OWL.Class):
+        if prefix in str(cls):
+            id = cls.replace(prefix,'')
+            integer_id = int(id,16)
+            ont.add((cls, skos.notation, rdflib.Literal(integer_id)))
+            column, code = ont.value(cls, dc.identifier).split('||||')
+            print("Mapping (%s, %s) => %s"%(column, code, cls))
+            mappings[(column, code)] = cls
+    with open(ontology,'wb') as o:
+        ont.serialize(o, format="turtle")
+
+    codebook = wb[infosheet_dict['Codebook'][1:]]
+
+    header = None
+    code_col = None
+    class_col = None
+    column_col = None
+    for row in codebook.rows:
+        if header is None:
+            header = row
+            for cell in header:
+                if cell.value == "Code":
+                    code_col = cell.column-1
+                if cell.value == "Class":
+                    class_col = cell.column-1
+                if cell.value == "Column":
+                    column_col = cell.column-1
+            print('Found Column Headers: Code==%s Class==%s Column=%s'%(code_col,class_col,column_col))
+            continue
+        if (row[column_col].value == None): continue
+        column = str(row[column_col].value)
+        code = str(row[code_col].value)
+        if (column, code) in mappings:
+            print("%s.%s = %s"%(column, code, mappings[(column, code)]))
+            row[class_col].value = mappings[(column, code)]
 
     wb.save(outputfile)
